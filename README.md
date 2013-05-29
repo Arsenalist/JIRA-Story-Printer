5a3591a965b0389960eff0d23d5b6d60a12abf6b
Zarar Siddiqi <zarars@gmail.com>
2013-05-29 14:37:18 -0400

    Add extension files.

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 JIRA-Story-Printer
 ==================
 
-This is a Google Chrome extension that makes cards of your JIRA story and sub-tasks.
+This is a Google Chrome extension that makes cards of your JIRA story and tasks (sub-tasks) so that they can be nicely printed for the Scrum board.
+
+It is available in the Google Chrome Web Store here: 
+
+https://chrome.google.com/webstore/detail/pojeijneialnccilaaegoiihakfocgea
---
+++ js/content_script.js
@@ -0,0 +1,84 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+  	if (!isStoryPage()) {
+  		alert("Easy there, Columbo, this isn't a story page.");
+  		return;
+  	}
+  	var storyTemplate = getTemplate('templates/story.html');
+  	var template = _.template(storyTemplate);
+  	$('html').html(template(getIssueInfo()));
+  	setTaskBoxHeights();
+
+  });
+
+
+
+function isStoryPage() {
+	var type = $('#type-val').justText();	
+	return type == 'Story'
+}
+
+function setTaskBoxHeights() {
+	var boxes = $('.task-well');
+	var maxHeight = Math.max.apply(
+	  Math, boxes.map(function() {
+	    return $(this).height();
+	}).get());
+	boxes.height(maxHeight);
+}
+function getTemplate(template) {
+	var text;
+  	$.ajax(chrome.extension.getURL(template), {async: false}).done(function(responseText) {
+  		text = responseText;
+  	});
+  	return text;
+}
+
+function getIssueInfo() {
+	return {
+		'story': getStoryInfo(),
+		'tasks': getTasks()
+	}
+}
+
+function getStoryInfo() {
+	var toStripFromDescription = /Click to add description/g
+	return {
+		'key': $('#key-val').justText().split('-')[1],
+		'summary': $('#summary-val').justText(),
+		'description': $('#description-val .user-content-block').html().replace(toStripFromDescription, ''),
+		'points': $('strong[title="Story Points"]').next().justText(),
+		'assignee': $('#assignee-val .user-hover').justText(),
+		'epic': $("dt:contains('Epic:')").next().find('a').justText(),
+		'estimate': $('#tt_aggregate_values_orig').justText()
+	}
+}
+
+function getTasks() {
+	var tasks = [];
+	$('#issuetable > tbody > tr').each(function(idx, tr) {
+		var tr = $(tr);
+		var estimate = tr.find('img[title*="Original Estimate"]').attr('alt');
+		var task = {
+			'key': tr.attr('data-issuekey').split('-')[1],
+			'summary': tr.find('td.stsummary a').justText(),
+			'assignee': tr.find('td.assignee span a').justText(),
+			'estimate': estimate ? estimate.split('-')[1] : null
+			
+		};
+		tasks.push(task);
+	});
+	return tasks;
+}
+
+
+jQuery.fn.justText = function() {
+   
+    return $(this).clone()
+            .children()
+            .remove()
+            .end()
+            .text()
+            .trim();
+ 
+};
